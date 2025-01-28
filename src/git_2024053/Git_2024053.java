aaa38df06a1818ecc29f45ac8f529c436e6ba75e
gssenna <guilhermesenna1997@gmail.com>
2025-01-28 12:44:33 +0000

    Second Commit

--- src/git_2024053/Git_2024053.java
+++ src/git_2024053/Git_2024053.java
@@ -4,6 +4,8 @@
  */
 package git_2024053;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Guilherme
@@ -14,7 +16,35 @@ public class Git_2024053 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Guilherme dos Santos Senna 2024053");
+         //Name: Guilherme dos Santos Senna 
+        //Sudent Number: 2024053
+        
+        //Creating a "Sign up" page for a digital platform.
+        //It asks the user to type the name in order to create an username.
+        
+        Scanner scanner = new Scanner(System.in); // creating a scanner in order to input something
+        
+        String name; // variable to store name
+       
+        System.out.println("please, insert your name for creating a username. (PS: no numbers or special characteres allowed!)"); // output the question
+        
+        name = scanner.nextLine(); // creates the first input
+        
+        while (!name.matches("^[a-zA-Z]+$")) { // loop  created if the user does not type name only
+            
+            System.out.println("Error, your name must not include numbers or special characteres!"); // error output
+            
+            name = scanner.nextLine(); // create a user input in case the user does not time a properly name
+        }
+
+        // Modifying the string "name":
+        
+        name = name.toUpperCase(); // Converts the name to uppercase 
+        
+        name = "@" + name;   // add a "@" in front of the name
+
+        // outpuut the name with all the modifications:
+        System.out.println("Your username is: " + name);
     }
     
 }
